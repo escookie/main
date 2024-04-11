278c87b1284a59636c0cbc576c0e788bbca0ad66
choisojin <choisojin@naver.com>
2024-04-11 11:18:13 +0900

    mobile works

--- _scripts/animated-masonry-gallery.js
+++ _scripts/animated-masonry-gallery.js
@@ -3,14 +3,8 @@ $(window).load(function () {
 var size = 1;
 var button = 1;
 var $container = $('#gallery-content-center');
-
-
   
 $container.isotope({itemSelector : 'img'});
-
-
-
-
 
 function check_button(){
 	
@@ -42,8 +36,6 @@ function check_button(){
 		$("#filter-bro");
 		}
 }
-
-
 	
 	$("#filter-landing").click(function() { $container.isotope({ filter: '.landing' }); button = 1; check_button(); }); //this.style.display='none'
 	$("#filter-site").click(function() {  $container.isotope({ filter: '.site' }); button = 2; check_button();  });
@@ -56,21 +48,19 @@ function check_button(){
 	$("#filter-bro").click(function() {  $container.isotope({ filter: '.bro' }); button = 9; check_button();  });
 	
 
-
-
 check_button();
 	
 });
 
 function openCity(evt, cityName) {
-	var i, tabcontent, tablinks;
-	tabcontent = document.getElementsByClassName("tabcontent");
-	for (i = 0; i < tabcontent.length; i++) {
-		tabcontent[i].style.display = "none";
+	var i, tabContent, tabLinks;
+	tabContent = document.getElementsByClassName("tabContent");
+	for (i = 0; i < tabContent.length; i++) {
+		tabContent[i].style.display = "none";
 	}
-	tablinks = document.getElementsByClassName("tablinks");
-	for (i = 0; i < tablinks.length; i++) {
-		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	tabLinks = document.getElementsByClassName("tabLinks");
+	for (i = 0; i < tabLinks.length; i++) {
+		tabLinks[i].className = tabLinks[i].className.replace(" active", "");
 	}
 	document.getElementById(cityName).style.display = "block";
 	evt.currentTarget.className += " active";
--- js/function.js
+++ js/function.js
@@ -8,7 +8,6 @@ $(function(){
   //var
   var $header = $('header');
   var $mnu = $('header>.container>nav>.gnb>li>a');
-  var $tag = $('#aboutme>.content1-right>.tag>ul>li>a');
   var scrollTop = 0;
   var nowIdx = 0;
   var arrTopVal = [];
@@ -42,163 +41,15 @@ $(function(){
         $mnu.eq(i).parent().addClass('on').siblings().removeClass('on');
       }
     }
-  });//end of header ecent
+  });//end of header
 
-  //tag
-  $tag.on('click',function(event){
-    tagIdx = $(this).attr('href');
-
-    if(tagIdx=='sub.html'){
-      $('#aboutme>.content1-left>a').trigger("click");
-
-    }else{
-      event.preventDefault();
-
-      $('html,body').stop().animate({
-        scrollTop : arrTopVal[tagIdx]
-      },500,'easeInOutCubic');
-    }
-  });
+  if ($(window).width() < 768) {
+    // 모바일 화면에서 추가적인 작업 수행
+    // 예를 들어, 메뉴를 터치할 때 메뉴를 숨기는 기능 추가 등
+  }
 
 });//end of header handler
 
-$(function(){
-  //var
-  var $mePrev = $('#aboutme>.content1-right>.profile>.prev');
-  var $meNext = $('#aboutme>.content1-right>.profile>.next');
-  var $aboutme = $('#aboutme>.content1-right>.profile>ul>li');
-  var $tag = $('#aboutme>.content1-right>.tag>ul>li');
 
-  var $list = $('#portfolio>.container>.mnu>li>a');
-  var $listImg = $('#portfolio>.container>.view>li');
-  var $viewOpen = $('.viewOpen');
-  var $viewClose = $('.viewClose');
-  var $viewImg = $('#portfolio>.portfolio_bg>.portfolio_img');
-  var $view = $('#portfolio>.portfolio_bg');
-
-  var $dePrev = $('#design>.prev');
-  var $deNext = $('#design>.next');
-  var $design = $('#design>.container>ul');
-  var $designs = $('#design>.container>ul>li');
-  var $gallOpen = $('.gallOpen');
-  var $gallClose = $('.gallClose');
-  var $gall = $('#design>.gallery_bg');
-  var $gallImg = $('#design>.gallery_bg>.gallery_img');
-
-  var nowIdx = 0;
-
-  //about me
-  $mePrev.on('click',function(){
-    if(nowIdx>0){
-      nowIdx--;
-      $mePrev.addClass('on');
-    }else{
-      $mePrev.removeClass('on');
-      $meNext.addClass('on');
-    }
-
-    $aboutme.eq(nowIdx).fadeIn().siblings().fadeOut();
-  });
-
-  $meNext.on('click',function(){
-    if(nowIdx<2){
-      nowIdx++;
-      $meNext.addClass('on');
-    }else{
-      nowIdx = 2;
-      $meNext.removeClass('on');
-      $mePrev.addClass('on');
-    }
-
-    $aboutme.eq(nowIdx).fadeIn().siblings().fadeOut();
-  });
-
-  //aboutme random tag
-  $(window).on('load',function(){
-    var randNum = Math.floor(Math.random()*9);
-    $tag.eq(randNum).addClass('on').siblings().removeClass('on');
-  });//end of about me
-
-  //portfolio
-  $list.on('click',function(event){
-    event.preventDefault();
-    nowIdx = $list.index(this);
-
-    $list.eq(nowIdx).parent().addClass('on').siblings().removeClass('on');
-
-    $listImg.fadeOut();
-    $listImg.eq(nowIdx).fadeIn();
-  });
-
-
-  $viewOpen.on('click',function(event){
-
-    event.preventDefault();
-    var src = $(this).attr('href');
-
-    $viewImg.find('a').css({
-      backgroundImage : 'url('+src+')'
-    });
-
-    $viewImg.parent().fadeIn();
-  });
-
-  $viewClose.on('click',function(event){
-    event.preventDefault();
-    $viewImg.scrollTop(0)
-    $view.fadeOut();
-  });
-
-  $view.on('click',function(){
-    $viewImg.scrollTop(0)
-    $view.fadeOut();
-  });
-  //end of portfolio
-
-  //design
-  function galleryMove(){
-    $designs.eq(nowIdx).stop().animate({left:0,},500,function(){
-      $designs.eq(nowIdx).siblings().css({'left':'990px'}).appendTo($design);
-    });
-  }
-
-  $dePrev.on('click',function(){
-    if(nowIdx>0){
-      nowIdx--;
-    }else{
-      nowIdx = 2;
-    }
-
-    galleryMove();
-  });
-
-  $deNext.on('click',function(){
-    if(nowIdx<2){
-      nowIdx++;
-    }else{
-      nowIdx = 0 ;
-    }
-
-    galleryMove();
-  });
-
-  $gallOpen.on('click',function(event){
-    event.preventDefault();
-    var src = $(this).attr('href');
-
-    $gallImg.css({
-      backgroundImage : 'url('+src+')'
-    }).parent().fadeIn();
-  });
-
-  $gallClose.on('click',function(event){
-    event.preventDefault();
-    $gall.fadeOut();
-  });
-
-  $gall.on('click',function(){
-    $gall.fadeOut();
-  });//end of design
-});//end of section handler
 
 
